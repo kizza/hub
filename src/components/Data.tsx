1cd90789eb661aa4871ab2c64fda989b48120987
Keiran <keiran.oleary@stacktracehq.com>
2019-10-14 17:30:28 +1000

    Display raw data within List

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,5 +1,15 @@
+import {
+  Collapse,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import React from "react";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StorageIcon from "@material-ui/icons/Storage";
+import React, { useState } from "react";
 import { MoistureLevel } from "../models";
 
 interface Data {
@@ -10,7 +20,7 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     box: {
       marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(2)
+      marginBottom: theme.spacing(1)
     }
   })
 );
@@ -20,15 +30,35 @@ const formatDate = (date: string) => new Date(date).toString();
 const Data: React.SFC<Data> = ({ items }) => {
   const { box } = useStyles();
 
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className={box}>
-      <ul>
-        {items.map((item: MoistureLevel) => (
-          <li key={item.objectId}>
-            {item.Sensor} {item.Value} {formatDate(item.createdAt)}
-          </li>
-        ))}
-      </ul>
+      <List>
+        <ListItem button onClick={handleClick}>
+          <ListItemIcon>
+            <StorageIcon />
+          </ListItemIcon>
+          <ListItemText primary="Raw data" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {items.map((item: MoistureLevel) => (
+              <ListItem key={item.objectId}>
+                <ListItemText
+                  primary={item.Sensor + " " + item.Value}
+                  secondary={formatDate(item.createdAt)}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Collapse>
+      </List>
     </div>
   );
 };
